8b49ac1d37870cc845d555b937dbebbff1b03bf6
Dave Rolsky <autarch@urth.org>
2013-07-10 11:33:43 -0500

    Move maps of type nums to/from strings to Common

--- lib/MaxMind/DB/Common.pm
+++ lib/MaxMind/DB/Common.pm
@@ -11,7 +11,34 @@ use constant {
 
 use Exporter qw( import );
 
-our @EXPORT_OK = qw( LEFT_RECORD RIGHT_RECORD DATA_SECTION_SEPARATOR_SIZE );
+our %TypeNumToString = (
+    0  => 'extended',
+    1  => 'pointer',
+    2  => 'utf8_string',
+    3  => 'double',
+    4  => 'bytes',
+    5  => 'uint16',
+    6  => 'uint32',
+    7  => 'map',
+    8  => 'int32',
+    9  => 'uint64',
+    10 => 'uint128',
+    11 => 'array',
+    12 => 'container',
+    13 => 'end_marker',
+    14 => 'boolean',
+    15 => 'float',
+);
+
+our %TypeStringToNum = reverse %TypeNumToString;
+
+our @EXPORT_OK = qw(
+    LEFT_RECORD
+    RIGHT_RECORD
+    DATA_SECTION_SEPARATOR_SIZE
+    %TypeNumToString
+    %TypeStringToNum
+);
 
 1;
 
